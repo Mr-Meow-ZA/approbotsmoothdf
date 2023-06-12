574546e6e88942e9b655776f567995b3599413d7
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2023-06-12 15:21:35 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,78 @@
-basic.forever(function () {
+function slowServo3 (deg3: number) {
+    if (deg3 > servoDeg3) {
+        servoDeg3 += 1
+        robotbit.Servo(robotbit.Servos.S3, servoDeg3)
+        basic.pause(10)
+    } else if (deg3 < servoDeg3) {
+        servoDeg3 += -1
+        robotbit.Servo(robotbit.Servos.S3, servoDeg3)
+        basic.pause(10)
+    } else {
+    	
+    }
+}
+function slowServo1 (deg1: number) {
+    if (deg1 > servoDeg1) {
+        servoDeg1 += 1
+        robotbit.Servo(robotbit.Servos.S1, servoDeg1)
+        basic.pause(10)
+    } else if (deg1 < servoDeg1) {
+        servoDeg1 += -1
+        robotbit.Servo(robotbit.Servos.S1, servoDeg1)
+        basic.pause(10)
+    } else {
+    	
+    }
+}
+input.onButtonPressed(Button.A, function () {
 	
+})
+function slowServo4 (deg4: number) {
+    if (deg4 > servoDeg4) {
+        servoDeg4 += 1
+        robotbit.Servo(robotbit.Servos.S4, servoDeg4)
+        basic.pause(10)
+    } else if (deg4 < servoDeg4) {
+        servoDeg4 += -1
+        robotbit.Servo(robotbit.Servos.S4, servoDeg4)
+        basic.pause(10)
+    } else {
+    	
+    }
+}
+microbitApp.onButtonUpOption(ButtonOption.Up, function () {
+    robotbit.Servo(robotbit.Servos.S4, 180)
+})
+microbitApp.onButtonOption(ButtonOption.Up, function () {
+    robotbit.Servo(robotbit.Servos.S4, 75)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+function slowServo2 (deg2: number) {
+    if (deg2 > servoDeg2) {
+        servoDeg2 += 1
+        robotbit.Servo(robotbit.Servos.S2, servoDeg2)
+        basic.pause(10)
+    } else if (deg2 < servoDeg2) {
+        servoDeg2 += -1
+        robotbit.Servo(robotbit.Servos.S2, servoDeg2)
+        basic.pause(10)
+    } else {
+    	
+    }
+}
+let servoDeg2 = 0
+let servoDeg4 = 0
+let servoDeg3 = 0
+let servoDeg1 = 0
+robotbit.Servo(robotbit.Servos.S1, 90)
+robotbit.Servo(robotbit.Servos.S2, 90)
+robotbit.Servo(robotbit.Servos.S3, 90)
+robotbit.Servo(robotbit.Servos.S4, 90)
+servoDeg1 = 90
+basic.forever(function () {
+    slowServo1(microbitApp.getSliderX())
+    slowServo2(microbitApp.getSliderY())
+    slowServo3(microbitApp.getSliderZ())
 })
